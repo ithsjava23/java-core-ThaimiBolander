2cfa758f147cd5611c640c75f561462a663f3d19
Thaimi Bolander <thaimi.bolander@iths.se>
2023-11-17 16:38:27 +0100

    Tests pass Lab 2.

--- src/main/java/org/example/warehouse/Category.java
+++ src/main/java/org/example/warehouse/Category.java
@@ -1,30 +1,65 @@
 package org.example.warehouse;
 
-public record Category(String name) {
-    private Category {
-        if(name ==  null) {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public final class Category {
+    private final String name;
+    private static final Map<String, Category> categories = new HashMap<>();
+
+
+    private Category (String name) {
+        if (name == null) {
             throw new IllegalArgumentException("Category name can't be null");
         }
-        this.name = capitalizeFirstLetter(name);
+        name = capitalizeFirstLetter(name);
+        this.name = name;
     }
-    private String capitalizeFirstLetter (String inputName) {
-        if(inputName == null || inputName.isEmpty()){
-            return inputName;
-        }
-        return Character.toUpperCase(inputName.charAt(0)) + inputName.substring(1);
-    }
-    static Category category = Category.of("Example");
-    private Category(){
-        this(null);
-    }
-    public static Category of(String name) {
-        return new Category(name);
-    }
-    public boolean getName () {
-        return false;
-    }
-}
 
+    private static String capitalizeFirstLetter (String str) {
+        if (str.isEmpty()) {
+            return str;
+        }
+        return Character.toUpperCase(str.charAt(0)) + str.substring(1);
+    }
+
+    public static Category of (String name) {
+        if (categories.containsKey(name))
+            return categories.get(name);
+        var category = new Category(name);
+        categories.put(name, category);
+        return category;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public String name () {
+        return name;
+    }
+
+    @Override
+    public boolean equals (Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Category) obj;
+        return Objects.equals(this.name , that.name);
+    }
+
+    @Override
+    public int hashCode () {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString () {
+        return "Category[" +
+                "name=" + name + ']';
+    }
+
+}
 
 
 //TODO: DisplayName Category, = made record ✅
--- src/main/java/org/example/warehouse/Product.java
+++
@@ -1,8 +0,0 @@
-package org.example.warehouse;
-
-import org.example.warehouse.Category;
-
-import java.util.List;
-
-public record Product(String name, float price, List<Product> products) {
-}
